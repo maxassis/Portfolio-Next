1c81f58ecf0bf9672df5758dca2358ff78bfd6af
maxassis <max.assis@ymail.com>
2021-02-07 17:25:25 -0300

    inicianco animaçoes

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 export const Wrapper = styled.main`
   width: 100%;
   height: 5rem;
-  background-color: #181818;
+  background-color: #252525;
   display: flex;
   justify-content: space-between;
   padding: 0 60px;
   align-items: center;
+  opacity: 0.9;
 `
 
 export const NameText = styled.h1`
